8bff647070308c1f87ec76199b24cf819b8eabee
Michael Boulton <michaelboulton@gmail.com>
2020-10-09 18:17:47 +0100

    Diff

--- tavern/request/base.py
+++ tavern/request/base.py
@@ -1,7 +1,14 @@
 from abc import abstractmethod
 
+from box import Box
+
 
 class BaseRequest(object):
     @abstractmethod
     def run(self):
         """Run test"""
+
+    @property
+    @abstractmethod
+    def request_vars(self) -> Box:
+        """Get any extra variables used for this request"""
