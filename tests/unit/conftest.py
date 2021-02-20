6ce1ac16f803fa7ab1c451e749f91d72f07656ff
Michael Boulton <michaelboulton@gmail.com>
2021-02-20 15:12:23 +0000

    Fix loading plugins before running tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -2,22 +2,28 @@ from unittest.mock import Mock
 
 import pytest
 
+from tavern.plugins import load_plugins
 from tavern.util.strict_util import StrictLevel
 
+includes = {
+    "variables": {
+        "request": {"prefix": "www.", "url": "google.com"},
+        "test_auth_token": "abc123",
+        "code": "def456",
+        "callback_url": "www.yahoo.co.uk",
+        "request_topic": "/abc",
+    },
+    "backends": {"mqtt": "paho-mqtt", "http": "requests"},
+    "strict": StrictLevel.all_on(),
+    "tavern_internal": {"pytest_hook_caller": Mock()},
+}
 
-@pytest.fixture(name="includes")
+
+@pytest.fixture(scope="function", name="includes")
 def fix_example_includes():
-    includes = {
-        "variables": {
-            "request": {"prefix": "www.", "url": "google.com"},
-            "test_auth_token": "abc123",
-            "code": "def456",
-            "callback_url": "www.yahoo.co.uk",
-            "request_topic": "/abc",
-        },
-        "backends": {"mqtt": "paho-mqtt", "http": "requests"},
-        "strict": StrictLevel.all_on(),
-        "tavern_internal": {"pytest_hook_caller": Mock()},
-    }
-
     return includes.copy()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def initialise_plugins():
+    load_plugins(includes)
